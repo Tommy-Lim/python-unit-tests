fe9cd02ea2d48af7fe0cc770185e6b5dce20bac2
Tommy-Lim <tlim24@gmail.com>
2017-02-07 15:15:47 -0800

    finished class definitions for bluray, books, cds, and digital items

--- items/digital_item.py
+++ items/digital_item.py
@@ -1,5 +1,14 @@
-# You are on your own... good luck.
 from .item import Item
 
 class DigitalItem(Item):
-    pass
+    def __init__(self, name, price):
+        super().__init__(name, price)
+        self.quantity = 1
+
+    def sell(self, amount):
+        self.quantity = 1
+        return True
+
+    def stock(self, amount):
+        self.quantity = 1
+        return True
